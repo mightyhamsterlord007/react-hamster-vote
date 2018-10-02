8f4d67d9e4e1b8a388d1e7cef8d7876b1bc9bdb0
Pak <Pak@admins-iMac-69.local>
2018-10-02 13:02:44 -0400

    add hamster.js

--- src/components/containers/HamsterList.js
+++ src/components/containers/HamsterList.js
@@ -6,13 +6,57 @@ import { generateVoteCount } from '../utils/randomNumGen';
 
 class HamsterList extends Component {
 
-
-
   render() {
- 
+
+    const hamstersArray = [
+      {
+        id: 1,
+        title: 'Hamster lord from East',
+        description: 'These nuts',
+        url: '#',
+        avatarUrl: 'images/users/kobe.jpeg',
+        hamsterImageUrl: 'images/hamsters/hamster-1.jpeg'
+      },
+      {
+        id: 2,
+        title: 'Hamster lord from South',
+        description: 'These nuts',
+        url: '#',
+        avatarUrl: 'images/users/michael.jpeg',
+        hamsterImageUrl: 'images/hamsters/hamster-2.jpeg'
+      },
+      {
+        id: 3,
+        title: 'Hamster lord from West',
+        description: 'These nuts',
+        url: '#',
+        avatarUrl: 'images/users/kevin.jpeg',
+        hamsterImageUrl: 'images/hamsters/hamster-3.jpeg'
+      },
+      {
+        id: 4,
+        title: 'Hamster lord from North',
+        description: 'These nuts',
+        url: '#',
+        avatarUrl: 'images/users/russell.jpeg',
+        hamsterImageUrl: 'images/hamsters/hamster-4.jpeg'
+      }
+    ];
+
+    let showHamstersInfo = hamstersArray.map((hamster) => {
+      return (
+          <div key={hamster.id}>
+              <img alt='' src={hamster.hamsterImageUrl} />
+              <h1>{hamster.title}</h1>
+              <span>{hamster.description}</span>
+              <img alt='' src={hamster.avatarUrl} />
+          </div>
+      )
+    })
+
     return (
      <div>
-      <Hamster />
+      {showHamstersInfo}
      </div>
     );
   }
--- src/components/views/Hamster.js
+++ src/components/views/Hamster.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 
 class Hamster extends Component {
- 
-
+  
   render() {
+
+  
+
     return (
-      <div>
-        Hamster
+      <div className='item'>
+        <div className='image'>
+          <img src='' alt='' />
+        </div>
+
+        <div className='middle aligned content'>
+          <div className='header'>
+            <a href='#'>
+              <i className='large caret up icon' />
+            </a>
+            50
+          </div>
+
+          <div className='description'>
+            <a href=''>
+              {this.props.title}
+            </a>
+          </div>
+
+
+
+        </div>
+
+
       </div>
     );
   }
